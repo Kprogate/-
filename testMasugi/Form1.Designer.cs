102535f947f7b7c75869fa2d6aa0a2583f6a5ecd
牧野　覚明 <g1010629@yahoo.co.jp>
2021-04-30 14:18:53 +0900

    横長

--- testMasugi/Form1.Designer.cs
+++ testMasugi/Form1.Designer.cs
@@ -46,9 +46,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 443);
+            this.ClientSize = new System.Drawing.Size(1173, 443);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
